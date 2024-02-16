8285f275b6dee87b61bd33cde2d9e07e0dd91ff4
Georgie <georgiehackett47@gmail.com>
2024-02-16 03:14:42 +0000

    Add event listener for search submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,39 @@
-//  Build the URL to query the Geocoding database
-var apiKey = "c2f29652dc947b9ba917f2a8f2a9b7e9";
-var city = "London";
-var geoLocationURL =
-  "http://api.openweathermap.org/geo/1.0/direct?q=" +
-  city +
-  "&limit=1&appid=" +
-  apiKey;
+document.addEventListener('DOMContentLoaded', (event) => {
+  
+var searchBtn = document.getElementById('search-button');
+var todayEl = document.getElementById('today');
+var forecastEl = document.getElementById('forecast');
 
-// Run Fetch call to the Geocoding API
- fetch(geoLocationURL)
+//  Build the URL to query the database
+var apiKey = "c2f29652dc947b9ba917f2a8f2a9b7e9";
+
+// var city = '';
+
+
+searchBtn.submit(searchWeather());
+
+function searchWeather() {
+  event.preventDefault();
+
+  var userInput = document.getElementById('search-input').value;
+
+  var forecastQueryURL =
+"http://api.openweathermap.org/data/2.5/forecast?q=" + "London" + "&appid=" + apiKey;
+console.log(forecastQueryURL);
+
+// console.log(userInput);
+
+  // Run Fetch call to the forecast API
+  fetch(forecastQueryURL)
   .then(function getCity(response) {
     // Call .json() to access the json data stored inside the returned promise
     return response.json();
   })
-  // Store the retrieved data inside of an object called "cityData"
-  .then(function(cityData) {
+  // Store the retrieved data inside of an object called "forecastData"
+  .then(function (forecastData) {
     // Log the resulting object
-    console.log(cityData);
+    console.log(forecastData);
 
-    // Store response data from our request in variables
-    var lon = cityData[0].lon.toString();
-    // console.log(lon);
-
-    var lat = cityData[0].lat.toString();
-    // console.log(lat);
-    
-    // Build the URL to query the 5 day forecast database
-    forecastQueryURL =
-      "http://api.openweathermap.org/data/2.5/forecast?lat=" +
-      lat +
-      "&lon=" +
-      lon +
-      "&appid=" +
-      apiKey;
-
-    return forecastQueryURL;
-  })
-  .then(function(data) {
-    console.log(forecastQueryURL)})
+  });
+}
+});
